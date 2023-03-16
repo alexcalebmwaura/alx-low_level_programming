edcd52e61d7a64c10b4ba5414e72099cca2fe3e7
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-16 14:44:20 -0700

    Task done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-include<>
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - This is a code
+ * Return: This returs 0
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
