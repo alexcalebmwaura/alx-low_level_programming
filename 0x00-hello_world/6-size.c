00c1f80e0d38dd691ed2c9f845e8a541fa1acace
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-16 15:31:37 -0700

    Task done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ printf("Size of int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
-return 0;
+return (0);
 }
