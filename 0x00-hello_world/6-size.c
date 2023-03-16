2d46bce3d496873f328c220d34908e9ee403c2d4
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-16 15:10:29 -0700

    Task done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - This is a code
+ * Return: Returns 0
+ */
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
+}
