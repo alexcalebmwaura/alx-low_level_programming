94dfd4216d3a51db1e3fb9ca410240b8f7efeb86
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-17 05:17:44 -0700

    Task done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description: Code run
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char alp[24] = "abcdfghijklmnoprstuvwxyz";
+int i;
+
+for (i = 0; i < 24; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
