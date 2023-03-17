2697f4bfdd4541df37c4e8a8a8d61d5ad432b165
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-17 11:51:07 -0700

    Task done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description: Code runs
+ * Returns: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
