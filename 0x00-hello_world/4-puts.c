23699db6cf2c1bf59303781c82608a1b130735ec
alexcalebmwaura@gmail.com <alexcalebmwaura@gmail.com>
2023-03-20 00:18:37 -0700

    Task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
 /**
- * main - This is Hello,put
- * Return: This returns 0
+ * main - Entry point
+ * description: Outputs comments
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
