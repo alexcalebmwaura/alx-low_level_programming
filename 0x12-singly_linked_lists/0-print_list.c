00db0b2e0a317d80b04209ca381992caeff7edda
alexcalebmwaura <alexcalebmwaura@gmail.com>
2023-04-27 18:52:32 +0000

    Task done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0](nil)\n");
-		}
 		else
-		{
 			printf("[%u]%s\n", h->len, h->str);
 			h = h->next;
-		}
-		list++;
+			list++;
 	}
 	return (list);
 }
